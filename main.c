859e2c76e8ebf2c6dd364b75b126b5d109d74ab6
Kel <ironcarbide@gmail.com>
2014-04-05 15:55:09 -0700

    Used kbhit() to exit if user input detected.
    During the loop, if keypress is registered then the loop will stop. This
    uses conio.h. From what I can tell online, it isn't a recommended thing to
    use because it is not the same across different OSes.

--- main.c
+++ main.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-
-
+#include <conio.h>
 
 int main()
 {
-    /*
-    int year = 0;
-    int month = 0;
-    printf("Welcome to the countdown timer.");
-    printf("What year were you born?");
-    scanf("%d", year)
-    printf("You entered %d. Is this correct? Y/N");
-
-    printf("What month were you born? (Enter the number)");
-    scanf("%d", month);
-    */
-
     int age = 0;
     int avglife = 80;
     int remainingyears = 0;
     unsigned long int remsec = 0;
+    int keypress = 0;
 
     printf("How old are you?\n");
     scanf("%d", &age);
@@ -38,11 +26,26 @@ int main()
 
     do{
         printf("\r                             ");
-        printf("\r%ld", remsec);
+        printf("\r%lu", remsec);
         Sleep(1000);
+        keypress = kbhit();
 
-    }while(remsec-- > 0);
+    }while((!keypress)&&(remsec-- > 0));
 
-
+    getchar();
+    printf("\n\nYou have exited. Hit enter to end.\n");
+    getchar();
     return 0;
 }
+
+    /*
+    int year = 0;
+    int month = 0;
+    printf("Welcome to the countdown timer.");
+    printf("What year were you born?");
+    scanf("%d", year)
+    printf("You entered %d. Is this correct? Y/N");
+
+    printf("What month were you born? (Enter the number)");
+    scanf("%d", month);
+    */
